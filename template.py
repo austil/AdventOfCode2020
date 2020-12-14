38c8b7e6a639c1f63ce2b1e384200130bd2f6308
austil <augustin@mrlacour.fr>
2020-12-14 17:49:19 +0100

    daily template

---
+++ template.py
@@ -0,0 +1,23 @@
+# Day __ : __
+#
+# Input : __
+# Part 1 : __
+
+import unittest
+from typing import List
+
+class SamplesTests(unittest.TestCase):
+
+    def test_part_one(self):
+        self.assertEqual(0, 0)
+    
+    # def test_part_two(self):
+    #     self.assertEqual(0, 0)
+
+unittest.main(argv=[''], verbosity=2, exit=False)
+
+print("\n------\n")
+
+# with open("puzzle_inputs/day.txt", "r") as f:
+    # input = f.readlines()
+    # print('Part 1:', )
