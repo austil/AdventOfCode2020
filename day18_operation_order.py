e5cc6f99eeb54516a5351bf0f50bae228a1c91e1
austil <augustin@mrlacour.fr>
2020-12-18 21:45:06 +0100

    day 18 - part 2 - dirty

--- day18_operation_order.py
+++ day18_operation_order.py
@@ -30,18 +30,64 @@ def compute(operation):
     assert len(stack) == 1
     return int(stack[0])
 
+def compute2(operation):
+    parts = operation.replace('(', ' ( ').replace(')', ' ) ').split(' ')
+    stack = []
+    for p in parts:
+        if p == ')':
+            while ( len(stack) >= 3
+                and stack[-3].isdigit()
+                and stack[-2] in ['*']
+                and stack[-1].isdigit()):
+                # apply operator as we go, from left to right
+                expr = f'{stack[-3]} {stack[-2]} {stack[-1]}'
+                stack = stack[:-3]
+                stack.append(str(eval(expr)))
+            # remove parenthesis
+            assert stack[-2] == '(' and stack[-1].isdigit()
+            stack = stack[:-2] + [stack[-1]]
+        elif p != '':
+            # stack
+            stack.append(p)
+        
+        while ( len(stack) >= 3
+            and stack[-3].isdigit()
+            and stack[-2] in ['+']
+            and stack[-1].isdigit()):
+            # apply operator as we go, from left to right
+            expr = f'{stack[-3]} {stack[-2]} {stack[-1]}'
+            stack = stack[:-3]
+            stack.append(str(eval(expr)))
+        
+    while ( len(stack) >= 3
+        and stack[-3].isdigit()
+        and stack[-2] in ['*']
+        and stack[-1].isdigit()):
+        # apply operator as we go, from left to right
+        expr = f'{stack[-3]} {stack[-2]} {stack[-1]}'
+        stack = stack[:-3]
+        stack.append(str(eval(expr)))
+
+    assert len(stack) == 1
+    return int(stack[0])
+
 class SamplesTests(unittest.TestCase):
 
     def test_part_one(self):
         self.assertEqual(compute('1 + 2 * 3 + 4 * 5 + 6'), 71)
-        self.assertEqual(compute('2 * 3 + (4 * 5)'), 26)
         self.assertEqual(compute('1 + (2 * 3) + (4 * (5 + 6))'), 51)
+        self.assertEqual(compute('2 * 3 + (4 * 5)'), 26)
         self.assertEqual(compute('5 + (8 * 3 + 9 + 3 * 4 * 3)'), 437)
         self.assertEqual(compute('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'), 12240)
         self.assertEqual(compute('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'), 13632)
     
-    # def test_part_two(self):
-    #     self.assertEqual(0, 0)
+    def test_part_two(self):
+        self.assertEqual(compute2('1 + 2 * 3 + 4 * 5 + 6'), 231)
+        self.assertEqual(compute2('1 + (2 * 3) + (4 * (5 + 6))'), 51)
+        self.assertEqual(compute2('2 * 3 + (4 * 5)'), 46)
+        self.assertEqual(compute2('5 + (8 * 3 + 9 + 3 * 4 * 3)'), 1445)
+        self.assertEqual(compute2('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'), 669060)
+        self.assertEqual(compute2('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'), 23340)
 
 unittest.main(argv=[''], verbosity=2, exit=False)
 
@@ -51,3 +97,4 @@ with open("puzzle_inputs/day18.txt", "r") as f:
     input = f.readlines()
     print('Part 1:', sum([compute(e.strip()) for e in input]))
     assert sum([compute(e.strip()) for e in input]) == 9535936849815
+    print('Part 2:', sum([compute2(e.strip()) for e in input]))
